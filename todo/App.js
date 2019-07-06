99a6da784909ad9cb677663a5c07b216484fbb41
Gabriela <balicas.gb@gmail.com>
2019-07-06 12:19:33 -0700

    update styling on main.js to try and emulate web css, need to improve layout next

--- todo/App.js
+++ todo/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Main from './app/components/Main';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View> 
+      <View>
+        <Main />
+      </View>
+    </View>
   );
 }
 
---
+++ todo/app/components/Main.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  Touchable,
+} from 'react-native';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}> RANDOM TEXT </Text>
+        </View>
+        <ScrollView style={styles.scrollContainer}>
+
+        </ScrollView>
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder='>note'
+            placeholderTextColor='white'>
+
+          </TextInput>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // color: 'red',
+  },
+  header: {
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: '#ddd',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    padding: 26,
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0, 
+    right: 0,
+    zIndex: 10,
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 10, 
+    backgroundColor: '#252525',
+    borderTopWidth: 2, 
+    borderTopColor: '#ededed'
+  },
+});
